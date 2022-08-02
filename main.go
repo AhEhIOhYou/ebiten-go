5b24a108f16281b7dd0812a7a510503db89259f9
ahehiohyou <diman_vladimirov34@mail.ru>
2022-08-02 16:03:43 +0300

    a-1

--- main.go
+++ main.go
@@ -1,31 +1,102 @@
 package main
 
 import (
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"fmt"
 	"golang.org/x/image/colornames"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
+	"image"
+	"image/color"
 	"log"
+	"strconv"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
+	"github.com/hajimehoshi/ebiten/v2/text"
 )
 
-type Game struct{}
+const (
+	screenWidth  = 640
+	screenHeight = 480
+)
+
+const sampleText = `The quick brown fox jumps over the lazy dog.`
+
+var (
+	mplusNormalFont font.Face
+	mplusBigFont    font.Face
+)
+
+func init() {
+	tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	const dpi = 72
+	mplusNormalFont, err = opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    24,
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	mplusBigFont, err = opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    48,
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Adjust the line height.
+	mplusBigFont = text.FaceWithLineHeight(mplusBigFont, 54)
+
+	myImage := image.NewRGBA(image.Rect(0, 0, 48, 48))
+
+	for i := 0; i < 48; i++ {
+		for j := 0; j < 48; j++ {
+			myImage.Set(i, j, colornames.White)
+		}
+	}
+
+	myImage.Set(9, 9, colornames.Red)
+
+	var img []image.Image
+	img = append(img, myImage)
+
+	ebiten.SetWindowIcon(img)
+}
+
+type Game struct {
+	counter int
+}
 
 func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(colornames.Red)
-	ebitenutil.DebugPrint(screen, "TEST")
+	const x = 20
+
+	// Draw info
+	msg := fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS())
+	text.Draw(screen, msg, mplusNormalFont, x, 40, color.White)
+
+	// Draw the sample text
+	text.Draw(screen, sampleText+strconv.Itoa(g.counter), mplusNormalFont, x, 80, color.White)
+	g.counter++
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
 }
 
 func main() {
-	ebiten.SetWindowSize(1280, 960)
-	ebiten.SetWindowTitle("ЙУХ")
-
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Font (Ebiten Demo)")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
