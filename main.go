0bef4c9bafa794efc2657472479436a6bccc2050
ahehiohyou <diman_vladimirov34@mail.ru>
2022-08-15 17:55:05 +0300

    make default prj config

--- main.go
+++ main.go
@@ -1,71 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
-	"github.com/hajimehoshi/ebiten/v2/text"
-	"golang.org/x/image/font"
-	"golang.org/x/image/font/opentype"
-	"image/color"
 	"log"
 )
 
 const (
 	screenWidth  = 640
-	screenHeight = 480
+	screenHeight = 400
 )
-
-const sampleText = `sample text`
-
-var (
-	mplusNormalFont font.Face
-	mplusBigFont    font.Face
-)
-
-func init() {
-	tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	const dpi = 72
-	mplusNormalFont, err = opentype.NewFace(tt, &opentype.FaceOptions{
-		Size:    24,
-		DPI:     dpi,
-		Hinting: font.HintingFull,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	mplusBigFont, err = opentype.NewFace(tt, &opentype.FaceOptions{
-		Size:    48,
-		DPI:     dpi,
-		Hinting: font.HintingFull,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	mplusBigFont = text.FaceWithLineHeight(mplusBigFont, 54)
-}
 
 type Game struct{}
 
-func (g *Game) Update() error {
+func (g Game) Update() error {
 	return nil
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {
-	const x = 20
+func (g Game) Draw(screen *ebiten.Image) {
 
-	msg := fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS())
-	text.Draw(screen, msg, mplusNormalFont, x, 40, color.White)
-
-	text.Draw(screen, sampleText, mplusNormalFont, x, 80, color.White)
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+func (g Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return screenWidth, screenHeight
 }
 
