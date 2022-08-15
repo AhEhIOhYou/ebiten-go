619a5daebec87aa29c622b278d69118ac6f58a34
ahehiohyou <diman_vladimirov34@mail.ru>
2022-08-15 17:48:30 +0300

    init commit

--- main.go
+++ main.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/image/colornames"
-	"golang.org/x/image/font"
-	"golang.org/x/image/font/opentype"
-	"image"
-	"image/color"
-	"log"
-	"strconv"
-
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
 	"github.com/hajimehoshi/ebiten/v2/text"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
+	"image/color"
+	"log"
 )
 
 const (
@@ -20,7 +16,7 @@ const (
 	screenHeight = 480
 )
 
-const sampleText = `The quick brown fox jumps over the lazy dog.`
+const sampleText = `sample text`
 
 var (
 	mplusNormalFont font.Face
@@ -51,28 +47,10 @@ func init() {
 		log.Fatal(err)
 	}
 
-	// Adjust the line height.
 	mplusBigFont = text.FaceWithLineHeight(mplusBigFont, 54)
-
-	myImage := image.NewRGBA(image.Rect(0, 0, 48, 48))
-
-	for i := 0; i < 48; i++ {
-		for j := 0; j < 48; j++ {
-			myImage.Set(i, j, colornames.White)
-		}
-	}
-
-	myImage.Set(9, 9, colornames.Red)
-
-	var img []image.Image
-	img = append(img, myImage)
-
-	ebiten.SetWindowIcon(img)
 }
 
-type Game struct {
-	counter int
-}
+type Game struct{}
 
 func (g *Game) Update() error {
 	return nil
@@ -81,13 +59,10 @@ func (g *Game) Update() error {
 func (g *Game) Draw(screen *ebiten.Image) {
 	const x = 20
 
-	// Draw info
 	msg := fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS())
 	text.Draw(screen, msg, mplusNormalFont, x, 40, color.White)
 
-	// Draw the sample text
-	text.Draw(screen, sampleText+strconv.Itoa(g.counter), mplusNormalFont, x, 80, color.White)
-	g.counter++
+	text.Draw(screen, sampleText, mplusNormalFont, x, 80, color.White)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -96,7 +71,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 
 func main() {
 	ebiten.SetWindowSize(screenWidth, screenHeight)
-	ebiten.SetWindowTitle("Font (Ebiten Demo)")
+	ebiten.SetWindowTitle("project2")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
