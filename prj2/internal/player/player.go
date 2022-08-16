32c08cf01f19829a85448dc6817628be34787009
ahehiohyou <diman_vladimirov34@mail.ru>
2022-08-16 13:42:41 +0300

    add player and actor

---
+++ prj2/internal/player/player.go
@@ -0,0 +1,36 @@
+package player
+
+import (
+	"bytes"
+	"github.com/AhEhIOhYou/project2/prj2/internal/actor"
+	"github.com/AhEhIOhYou/project2/prj2/internal/sprite"
+	"github.com/AhEhIOhYou/project2/prj2/resources/images"
+	"github.com/hajimehoshi/ebiten/v2"
+	"image"
+)
+
+// Player represents player of the game
+type Player struct {
+	sprite *sprite.Sprite
+	actor  *actor.Actor
+}
+
+// New returns initialized Player
+func New() *Player {
+	p := &Player{}
+
+	img, _, _ := image.Decode(bytes.NewReader(images.P_ROBO1))
+	sp := sprite.New(&img, 8)
+	p.sprite = sp
+
+	p.actor = &actor.Actor{}
+	p.actor.SetPosition(120, 160)
+
+	return p
+}
+
+// Draw draws this sprite
+func (p *Player) Draw(screen *ebiten.Image) {
+	p.sprite.SetPosition(p.actor.X, p.actor.Y)
+	p.sprite.Draw(screen)
+}
--- prj2/main.go
+++ prj2/main.go
@@ -1,19 +1,15 @@
 package prj2
 
 import (
-	"bytes"
-	"github.com/AhEhIOhYou/project2/prj2/internal/sprite"
-	"github.com/AhEhIOhYou/project2/prj2/resources/images"
+	"github.com/AhEhIOhYou/project2/prj2/internal/player"
 	"github.com/hajimehoshi/ebiten/v2"
-	"image"
 	_ "image/png"
-	"log"
 )
 
 var (
-	testSprite    *sprite.Sprite
-	screenWidth   = 1280
-	screenHeight  = 960
+	myPlayer      *player.Player
+	screenWidth   = 640
+	screenHeight  = 440
 	isInitialized = false
 )
 
@@ -22,24 +18,18 @@ type Game struct{}
 // Update updates a game by one tick. The given argument represents a screen image.
 func (g *Game) Update() error {
 	if !isInitialized {
-		img, _, err := image.Decode(bytes.NewReader(images.ESHOT10_1))
-		if err != nil {
-			log.Fatal(err)
-		}
-		testSprite = sprite.New(&img, 1)
-		testSprite.SetIndex(0)
+		myPlayer = player.New()
 		isInitialized = true
 		return nil
 	}
 
-	testSprite.SetPosition(float64(screenWidth/2), float64(screenHeight/2))
 	return nil
 }
 
 // Draw draws the game screen.
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
-	testSprite.Draw(screen)
+	myPlayer.Draw(screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
