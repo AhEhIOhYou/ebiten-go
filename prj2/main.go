2025695b1e15ca642a5c8db4379037d376033937
ahehiohyou <diman_vladimirov34@mail.ru>
2022-08-16 17:48:46 +0300

    add cute rectangle

--- prj2/main.go
+++ prj2/main.go
@@ -4,12 +4,14 @@ import (
 	"github.com/AhEhIOhYou/project2/prj2/internal/input"
 	"github.com/AhEhIOhYou/project2/prj2/internal/player"
 	"github.com/hajimehoshi/ebiten/v2"
+	"image/color"
 	_ "image/png"
 )
 
 var (
 	myGameInput   *input.GameInput
 	myPlayer      *player.Player
+	myInputPanel  *input.Panel
 	screenWidth   = 640
 	screenHeight  = 440
 	isInitialized = false
@@ -22,11 +24,13 @@ func (g *Game) Update() error {
 	if !isInitialized {
 		myPlayer = player.New()
 		myGameInput = input.New()
+		myInputPanel = input.NewPanel(screenWidth, screenHeight)
 		isInitialized = true
 		return nil
 	}
 
 	myGameInput.Update()
+	myInputPanel.UpdatePanel()
 	myPlayer.Update(myGameInput)
 
 	return nil
@@ -35,7 +39,9 @@ func (g *Game) Update() error {
 // Draw draws the game screen.
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.RGBA{R: 0x10, G: 0x10, B: 0x30, A: 0xff})
 	myPlayer.Draw(screen)
+	myInputPanel.DrawPanel(screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
