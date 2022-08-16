71f92e274be049ca500bba5b81fb89097742d1bb
ahehiohyou <diman_vladimirov34@mail.ru>
2022-08-16 16:45:20 +0300

    update moving

--- prj2/internal/input/input.go
+++ prj2/internal/input/input.go
@@ -1,38 +1,54 @@
 package input
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"time"
+)
 
 // GameInput represents the state of user's input
 type GameInput struct {
-	Up    float64
-	Left  float64
-	Down  float64
-	Right float64
-	Fire  bool
-	Focus bool
+	Up           float64
+	Left         float64
+	Down         float64
+	Right        float64
+	Fire         bool
+	Focus        bool
+	prevTickTime time.Time
+}
+
+func New() *GameInput {
+	gameInput := &GameInput{}
+	gameInput.prevTickTime = time.Now()
+	return gameInput
 }
 
 // Update updates the input state
 func (i *GameInput) Update() {
-	if ebiten.IsKeyPressed(ebiten.KeyW) || ebiten.IsKeyPressed(ebiten.KeyUp) {
+
+	if time.Since(i.prevTickTime).Milliseconds() < 50 {
+		return
+	}
+	i.prevTickTime = time.Now()
+
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
 		i.Up = 1
 	} else {
 		i.Up = 0
 	}
 
-	if ebiten.IsKeyPressed(ebiten.KeyA) || ebiten.IsKeyPressed(ebiten.KeyLeft) {
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
 		i.Left = 1
 	} else {
 		i.Left = 0
 	}
 
-	if ebiten.IsKeyPressed(ebiten.KeyS) || ebiten.IsKeyPressed(ebiten.KeyDown) {
+	if ebiten.IsKeyPressed(ebiten.KeyDown) {
 		i.Down = 1
 	} else {
 		i.Down = 0
 	}
 
-	if ebiten.IsKeyPressed(ebiten.KeyD) || ebiten.IsKeyPressed(ebiten.KeyRight) {
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
 		i.Right = 1
 	} else {
 		i.Right = 0
--- prj2/main.go
+++ prj2/main.go
@@ -21,7 +21,7 @@ type Game struct{}
 func (g *Game) Update() error {
 	if !isInitialized {
 		myPlayer = player.New()
-		myGameInput = &input.GameInput{}
+		myGameInput = input.New()
 		isInitialized = true
 		return nil
 	}
