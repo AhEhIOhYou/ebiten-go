1817693474c74eea489363895b75c169ce9ecafb
ahehiohyou <diman_vladimirov34@mail.ru>
2022-08-18 17:38:17 +0300

    update main scene

--- prj2/internal/scene/scene.go
+++ prj2/internal/scene/scene.go
@@ -1,11 +1,107 @@
 package scene
 
 import (
+	"github.com/AhEhIOhYou/project2/prj2/internal/scene/actors"
+	"github.com/AhEhIOhYou/project2/prj2/internal/scene/fields"
+	"github.com/AhEhIOhYou/project2/prj2/internal/scene/inputs"
+	"github.com/AhEhIOhYou/project2/prj2/internal/scene/tools"
 	"github.com/hajimehoshi/ebiten/v2"
+	"image/color"
+	"math/rand"
+	"time"
 )
 
-// Scene represents scene interface
-type Scene interface {
-	Update()
-	Draw(screen *ebiten.Image)
+const (
+	maxPlayerShot = 80
+)
+
+// PlayerShooter представляет интерфейс оружия игрока
+type PlayerShooter interface {
+	Shot(x, y float64, degree int, playerShots []*actors.PlayerBullet)
+}
+
+var (
+	input        *inputs.Input
+	field        *fields.Field
+	player       *actors.Player
+	playerWeapon PlayerShooter
+	playerShots  [maxPlayerShot]*actors.PlayerBullet
+)
+
+// Scene представляет сцену
+type Scene struct{}
+
+// NewSceneOptions представляет настройки сцены
+type NewSceneOptions struct {
+	ScreenWidth  int
+	ScreenHeight int
+}
+
+// NewScene вернет стандартную сцену
+func NewScene(options NewSceneOptions) *Scene {
+	stg := &Scene{}
+	initGame()
+	return stg
+}
+
+// initGame инициализирует игру
+func initGame() {
+	rand.Seed(time.Now().Unix())
+	input = inputs.New()
+	field = fields.NewField()
+
+	actors.SetBoundary(field)
+
+	player = actors.NewPlayer()
+	playerWeapon = &tools.PlayerWeapon{}
+
+	for i := 0; i < len(playerShots); i++ {
+		playerShots[i] = actors.NewPlayerShot()
+	}
+
+}
+
+// Update обновляет состояние сцены (актеров и окружения)
+func (stg *Scene) Update() {
+	input.Update()
+	checkCollision()
+	player.Action(input.Horizontal, input.Vertical, input.Fire, input.Focus)
+
+	if input.Fire {
+		x, y := player.GetPosition()
+		playerWeapon.Shot(x, y, player.GetNormalizedDegree(), playerShots[:])
+	}
+
+	for i := 0; i < len(playerShots); i++ {
+		p := playerShots[i]
+		if p.IsActive() == false {
+			continue
+		}
+		p.Move()
+	}
+}
+
+// Draw отрисовывает всех действиующих лиц сцены
+func (stg *Scene) Draw(screen *ebiten.Image) {
+	screen.Fill(color.RGBA{0x10, 0x10, 0x30, 0xff})
+	field.Draw(screen)
+	player.Draw(screen)
+
+	for i := 0; i < len(playerShots); i++ {
+		p := playerShots[i]
+		if p.IsActive() == false {
+			continue
+		}
+		p.Draw(screen)
+	}
+}
+
+// checkCollision Проверка столкновений
+func checkCollision() {
+	for i := 0; i < len(playerShots); i++ {
+		p := playerShots[i]
+		if p.IsActive() == false {
+			continue
+		}
+	}
 }
--- prj2/main.go
+++ prj2/main.go
@@ -2,33 +2,40 @@ package prj2
 
 import (
 	"github.com/AhEhIOhYou/project2/prj2/internal/scene"
-	"github.com/AhEhIOhYou/project2/prj2/internal/scene/shooting"
+	"github.com/AhEhIOhYou/project2/prj2/internal/sprite"
 	"image/color"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-var (
-	currentScene  scene.Scene = nil
-	screenWidth               = 640
-	screenHeight              = 440
-	isLayouted                = false
-	isInitialized             = false
-)
-
-// Game implements ebiten.Game interface.
 type Game struct{}
 
-// Update updates a game by one tick. The given argument represents a screen image.
+// Scene представляет интерфейс сцены
+type Scene interface {
+	Update()
+	Draw(screen *ebiten.Image)
+}
+
+var (
+	currentScene    Scene = nil
+	screenWidth           = 640
+	screenHeight          = 640
+	isInitialized         = false
+	isWindowSizeSet       = false
+)
+
+// Update обновляет сцену каждый тик (tps)
 func (g *Game) Update() error {
-	if isLayouted && !isInitialized {
-		currentScene = shooting.New(shooting.NewOptions(shooting.NewOptions{
+	if isWindowSizeSet && !isInitialized {
+		sprite.LoadSprites()
+		currentScene = scene.NewScene(scene.NewSceneOptions{
 			ScreenWidth:  screenWidth,
 			ScreenHeight: screenHeight,
-		}))
+		})
 		isInitialized = true
 		return nil
 	}
+
 	if currentScene != nil {
 		currentScene.Update()
 	}
@@ -36,22 +43,21 @@ func (g *Game) Update() error {
 	return nil
 }
 
-// Draw draws the game screen.
-// Draw is called every frame (typically 1/60[s] for 60Hz display).
+// Draw отрисовка сцены, вызывается каждый фрейм (fps)
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.RGBA{0x10, 0x10, 0x30, 0xff})
-	currentScene.Draw(screen)
+	if currentScene != nil {
+		currentScene.Draw(screen)
+	}
 }
 
-// Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
-// If you don't have to adjust the screen size with the outside size, just return a fixed size.
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	screenHeight = int(float64(screenWidth) / float64(outsideWidth) * float64(outsideHeight))
-	isLayouted = true
+	isWindowSizeSet = true
 	return screenWidth, screenHeight
 }
 
-// NewGame creates a game struct
+// NewGame создает новую стуктуры игры
 func NewGame() (*Game, error) {
 	game := &Game{}
 	return game, nil
