84a8a7162b9c14fd4119eaafb330e9064c56b9bd
ahehiohyou <diman_vladimirov34@mail.ru>
2022-08-23 21:36:54 +0300

    update weapon methods

--- prj2/internal/tools/weapon.go
+++ prj2/internal/tools/weapon.go
@@ -6,6 +6,10 @@ import (
 
 type Weapon interface {
 	Fire(x, y float64, degree int)
+	SetDegree(degree int)
+	GetDegree() int
+	SetCooldown(cooldown int64)
+	GetCooldown() int64
 }
 
 type shotFactoryFunction func(x, y float64, degree int)
@@ -13,4 +17,22 @@ type shotFactoryFunction func(x, y float64, degree int)
 type baseWeapon struct {
 	shotFactory  shotFactoryFunction
 	lastShotTime time.Time
+	degree       int
+	cooldown     int64
+}
+
+func (w *baseWeapon) SetDegree(degree int) {
+	w.degree = degree
+}
+
+func (w *baseWeapon) GetDegree() int {
+	return w.degree
+}
+
+func (w *baseWeapon) SetCooldown(cooldown int64) {
+	w.cooldown = cooldown
+}
+
+func (w *baseWeapon) GetCooldown() int64 {
+	return w.cooldown
 }
